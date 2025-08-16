c312044d23b496922a8e9a54d7a1027aeffc07bd
Darlington0433 <quannnd2004@gmail.com>
2025-08-16 10:48:53 +0700

    Refactor code structure for improved readability and maintainability

--- Semester1/PF1127_BasicProgramingLanguage/Session02VariableConstantConsoleInput-Output/A3/A3.cpp
+++ Semester1/PF1127_BasicProgramingLanguage/Session02VariableConstantConsoleInput-Output/A3/A3.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 int main() {
-    int A, B;
+    double A, B;
 
     cout << "Enter A: ";
     cin >> A;
-
     cout << "Enter B: ";
     cin >> B;
-    
-    if(A = 0) {
 
+    if (A == 0) {
+        if (B == 0) {
+            cout << "The equation is true for any number." << endl;
+        } else {
+            cout << "The equation has no root" << endl;
+        }
     } else {
-
+        double X = -B / A;
+        cout << "Solution of equation: " << X << endl;
     }
-
     return 0;
 }
