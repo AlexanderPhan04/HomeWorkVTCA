7d5a33d0a51613e5eee815fdf5ba2b3675cbbc10
Darlington0433 <quannnd2004@gmail.com>
2025-09-12 14:54:47 +0700

    Implement code changes to enhance functionality and improve performance

--- Semester1/PF1127_BasicProgramingLanguage/Lab9.Session09-Functions/A4/A4.cpp
+++ Semester1/PF1127_BasicProgramingLanguage/Lab9.Session09-Functions/A4/A4.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <limits>
 using namespace std;
+
+int getMenuChoice()
+{
+    int choice;
+    while (true)
+    {
+        cout << "==========================" << endl;
+        cout << "|         Menu           |" << endl;
+        cout << "==========================" << endl;
+        cout << "|1. Menu 1               |" << endl;
+        cout << "|2. Menu 2               |" << endl;
+        cout << "|3. Menu 3               |" << endl;
+        cout << "|4. Menu 4               |" << endl;
+        cout << "|5. Exit                 |" << endl;
+        cout << "==========================" << endl;
+        cout << "Please choose: ";
+        cin >> choice;
+
+        if (cin.fail() || choice < 1 || choice > 5)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max());
+            cout << "Entered incorrectly, re-enter." << endl;
+        }
+        else
+        {
+            return choice;
+        }
+    }
+}
 
 int main()
 {
-    
+    int choice;
+    do
+    {
+        choice = getMenuChoice();
+        switch (choice)
+        {
+        case 1:
+            cout << "Doing menu 1..." << endl;
+            break;
+        case 2:
+            cout << "Doing menu 2..." << endl;
+            break;
+            case 3:
+            cout << "Doing menu 3..." << endl;
+            break;
+            case 4:
+            cout << "Doing menu 4..." << endl;
+            break;
+            case 5:
+            cout << "Exiting program..." << endl;
+            break;
+        }
+    } while (choice != 5);
 
     return 0;
 }
